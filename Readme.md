127eef6a5fab9073c76624f6711436ef052d1fa6
Muhammad Faisal Tariq <mfaisaltariq@gmail.com>
2021-02-15 02:12:16 +0500

    working with scalar and custom arrays

--- Readme.md
+++ Readme.md
@@ -7,3 +7,5 @@
 3- Understanding Scalar Types </br>
 4- Creating Custom Types </br>
 5- Operation Arguments
+6- Working with Scalar Arrays
+7- Working with Custom Arrays
--- basics/src/index.js
+++ basics/src/index.js
@@ -1,10 +1,19 @@
 import {GraphQLServer} from 'graphql-yoga';
+import db from '../../data.json'
 
 // Type Definitions (Schemas)
+// type Query {
+//     greeting(name: String): String!
+//     grades: [Int!]!
+//     add(numbers: [Float!]!): Float!
+//     me: User!
+//     post: Post!
+// }
+
 const typeDefs = `
     type Query {
-        greeting(name: String): String!
-        add(num1: Float!, num2: Float!): Float!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
     }
@@ -27,11 +36,19 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
     Query: {
-        greeting(parent, args, ctx, info) {
-            return args.name ? `Hello ${args.name}!` : `Hello!`
-        } ,
-        add(parent, args, ctx, info) {
-            return args.num1 + args.num2
+        users(parent, args, ctx, info) {
+            if (!args.query) return db.users;
+
+            return db.users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase());
+            });
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) return db.posts;
+
+            return db.posts.filter((post) => {
+                return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
+            })
         },
         me() {
             return {
@@ -49,6 +66,21 @@ const resolvers = {
                 published: true
             }
         }
+
+
+        // greeting(parent, args, ctx, info) {
+        //     return args.name ? `Hello ${args.name}!` : `Hello!`
+        // } ,
+        // grades() {
+        //     return [98, 90, 91]
+        // },
+        // add(parent, args, ctx, info) {
+        //     if (args.numbers.length === 0) return 0;
+            
+        //     return args.numbers.reduce((accumulator, currentValue) => {
+        //         return accumulator + currentValue;
+        //     });
+        // },
     }
 }
 
